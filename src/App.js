3327f94793d6965848991223129ecb58b1e1f597
obuf <ahmadgharabli23@gmail.com>
2024-07-28 20:09:09 +0300

    Edit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+      <h1>MY FRIEND, THIS IS</h1>
+      <p>My BEST APP</p>
+      <div className="buy-btn">
+        <div className="first">Buy Me Now</div>
+        <div className="bold">ONLY 99.97$</div>
+      </div>
     </div>
   );
 }
